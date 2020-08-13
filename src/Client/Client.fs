df64f548bc34538cd4e65c9bbba271e0cda5cf08
Isaac Abraham <isaac.abraham@gmail.com>
2020-08-14 00:58:24 +0200

    Stronger typing

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -6,28 +6,28 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Feliz
 
+/// Represents a prop for the Auto Suggester
 type IAutoSuggestProp = interface end
 
+/// Creates an Auto Suggester tied to a specific type of value used to bind to suggestions with.
 [<Erase>]
-type AutoSuggest() =
+type AutoSuggest<'T> =
     static member inline create props = Interop.reactApi.createElement (importDefault "react-autosuggest", createObj !!props)
-
-    static member inline suggestions<'T>(items:seq<'T>) = Interop.mkAttr "suggestions" (Seq.toArray items)
-    static member inline getSuggestionValue<'T> (getter:'T -> string) = Interop.mkAttr "getSuggestionValue" getter
-    static member inline onSuggestionsFetchRequested<'T>(updater:'T -> unit) = Interop.mkAttr "onSuggestionsFetchRequested" (fun s -> updater s?value)
+    static member inline suggestions(items:seq<'T>) = Interop.mkAttr "suggestions" (Seq.toArray items)
+    static member inline getSuggestionValue(getter:'T -> string) = Interop.mkAttr "getSuggestionValue" getter
+    static member inline onSuggestionsFetchRequested(updater:string -> unit) = Interop.mkAttr "onSuggestionsFetchRequested" (fun s -> updater s?value)
     static member inline onSuggestionsClearRequested (clear:unit -> unit) = Interop.mkAttr "onSuggestionsClearRequested" clear
-    static member inline renderSuggestion<'T>(renderer:'T -> ReactElement) = Interop.mkAttr "renderSuggestion" renderer
-
+    static member inline renderSuggestion(renderer:'T -> ReactElement) = Interop.mkAttr "renderSuggestion" renderer
     static member inline inputProps (props:IAutoSuggestProp seq) = Interop.mkAttr "inputProps" (createObj !!props)
     static member inline value (value:string) = prop.value value |> unbox<IAutoSuggestProp>
     static member inline onChange (handler: string -> unit) = Interop.mkAttr "onChange" (System.Func<_,_,unit> (fun _ event -> handler event?newValue)) |> unbox<IAutoSuggestProp>
 
-type Suggestion = {| Original : string; Text : string |}
+type Tenant = {| Original : string; Text : string |}
 
 type Model =
     { Text: string
-      AllTenants : Suggestion array
-      Suggestions : Suggestion array }
+      AllTenants : Tenant array
+      Suggestions : Tenant array }
 
 type Msg =
     | TextChanged of string
@@ -35,7 +35,7 @@ type Msg =
     | ClearSuggestions
     | UpdateSuggestions of string
 
-let findSuggestions (allSuggestions:Suggestion array) (text:string) =
+let findSuggestions (allSuggestions:Tenant array) (text:string) =
     allSuggestions
     |> Seq.filter(fun s -> s.Text.Contains (text.ToLower()))
     |> Seq.truncate 10
@@ -58,22 +58,23 @@ let update msg (model:Model) =
 open Fable.React
 open Fable.React.Props
 
+type TenantSuggester = AutoSuggest<Tenant>
 let view model dispatch =
     div [] [
         img [ Src "favicon.png" ]
         h1 [] [ str "autosuggest" ]
-        AutoSuggest.create [
-            AutoSuggest.suggestions model.Suggestions
+        TenantSuggester.create [
+            TenantSuggester.suggestions model.Suggestions
 
-            AutoSuggest.getSuggestionValue(fun (s:Suggestion) -> s.Text)
-            AutoSuggest.renderSuggestion(fun (s:Suggestion) -> Html.span s.Original)
+            TenantSuggester.getSuggestionValue(fun s -> s.Original)
+            TenantSuggester.renderSuggestion(fun s -> Html.span s.Original)
 
-            AutoSuggest.onSuggestionsFetchRequested (UpdateSuggestions >> dispatch)
-            AutoSuggest.onSuggestionsClearRequested (fun () -> dispatch ClearSuggestions)
+            TenantSuggester.onSuggestionsFetchRequested (UpdateSuggestions >> dispatch)
+            TenantSuggester.onSuggestionsClearRequested (fun () -> dispatch ClearSuggestions)
 
-            AutoSuggest.inputProps [
-                AutoSuggest.value model.Text
-                AutoSuggest.onChange (TextChanged >> dispatch)
+            TenantSuggester.inputProps [
+                TenantSuggester.value model.Text
+                TenantSuggester.onChange (TextChanged >> dispatch)
             ]
         ]
     ]
